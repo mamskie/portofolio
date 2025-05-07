0cf114b181577e1cc44a6f4ba279c4f3dba8eafe
MAM <86782580+mamskie@users.noreply.github.com>
2025-05-07 11:15:02 +0700

    Update layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,7 +1,7 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Footer } from './footer';
 import { Header } from './header';
 import type { PropsWithChildren } from 'react';
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export function Layout({ children }: PropsWithChildren): JSX.Element {
   return (
