e3c2c24cca05b450ad7100dbc0952b7cd5931d59
MAM <86782580+mamskie@users.noreply.github.com>
2025-05-07 11:17:51 +0700

    Update layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,4 +1,4 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Footer } from './footer';
 import { Header } from './header';
 import type { PropsWithChildren } from 'react';
