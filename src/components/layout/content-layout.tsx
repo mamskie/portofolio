1a736944778ce8c1c3280fa6689b6ad675240028
MAM <86782580+mamskie@users.noreply.github.com>
2025-05-08 10:27:12 +0700

    Update content-layout.tsx

--- src/components/layout/content-layout.tsx
+++ src/components/layout/content-layout.tsx
@@ -41,8 +41,8 @@ export function ContentLayout({
 
   const contentIsBlog = type === 'blog';
 
-  const githubCommitHistoryUrl = `https://github.com/ccrsxx/portofolio/commits/main/src/pages/${type}/${slug}.mdx`;
-  const githubContentUrl = `https://github.com/ccrsxx/portofolio/blob/main/src/pages/${type}/${slug}.mdx`;
+  const githubCommitHistoryUrl = `https://github.com/mamskie/portofolio/commits/main/src/pages/${type}/${slug}.mdx`;
+  const githubContentUrl = `https://github.com/mamskie/portofolio/blob/main/src/pages/${type}/${slug}.mdx`;
 
   const article: Article = {
     type,
