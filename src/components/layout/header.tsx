77dea8a03270d2759d5a9bc5165f94b93f9efa23
MAM <86782580+mamskie@users.noreply.github.com>
2025-06-12 11:18:11 +0700

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -50,7 +50,7 @@ const navLinks = [
   { name: 'About', href: '/about' },
   { name: 'Resume', href: '/resume' },
   { name: 'Projects', href: '/projects' },
-  { name: 'Guestbook', href: '/guestbook' },
+  { name: 'Guestbook', href: '/guestbook' }
   // { name: 'Contact', href: '/contact' },
   // { name: 'Blog', href: '/blog' }
 ] as const;
