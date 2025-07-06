793dd026b73e4a2203b99f6af4b084b749796181
MAM <86782580+mamskie@users.noreply.github.com>
2025-07-07 06:12:09 +0700

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -48,7 +48,7 @@ export function Header(): JSX.Element {
 const navLinks = [
   { name: 'Home', href: '/' },
   { name: 'About', href: '/about' },
-  { name: 'Resume', href: '/resume' },
+  //{ name: 'Resume', href: '/resume' },
   { name: 'Projects', href: '/projects' },
   { name: 'Guestbook', href: '/guestbook' }
   // { name: 'Contact', href: '/contact' },
